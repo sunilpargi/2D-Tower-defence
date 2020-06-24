966706c4b21cc329127711d952f5e1f28bbef749
Sunil Pargi <sunilpargi121@gmail.com>
2020-06-24 16:55:01 +0530

    Completed UI Buttons

---
+++ Tower Defense/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : Singleton<TowerManager>
+{
+    private TowerBtn towerBtnPressed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void selectedButton(TowerBtn towerSelected)
+    {
+        towerBtnPressed = towerSelected;
+    }
+}
